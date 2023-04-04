660157a15e91d4a2cfacd69591f081f64cf21570
Matheus Pelucio <mts.rangel@hotmail.com>
2023-04-04 14:49:00 -0300

    Exibir posição das rodas em roda PORT+1

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
 const WebSocket = require('ws');
 const { WebSocketServer } = require('ws');
 
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const path = require('path');
+
 const PORT = 3000;
 const wsServer = new WebSocketServer({ port: PORT });
 
-let wellData // { FR_L, FR_R, RE_L, RE_R }
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+let wellData = {FR_L: 0, FR_R: 0, RE_L: 0, RE_R: 0}
+
+app.use('/', (req, res) => {
+    res.render('index.html')
+})
+
+io.on('connection', socket => {
+    socket.emit('receivedData', wellData)
+    //console.log(socket.id)
+})
+
 
 wsServer.on('connection', socket => {
     console.log("---> Simulator connnected");
 
     socket.on('message', data => {
         wellData = JSON.parse(data)
-        console.log("---> "  + wellData);
+        console.log("---> "  + data);
+        socket.emit('sendData', wellData);
+        io.emit('receivedData', wellData)
     });
 });
+
+server.listen(PORT+1)
 
 console.log(" Server is listening on port " + PORT);
